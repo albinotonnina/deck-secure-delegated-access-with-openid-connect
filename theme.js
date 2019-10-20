196b9c9fd628e0ffc7254e6da489c0a058682d16
Albino Tonnina <albino.tonnina@asos.com>
2019-10-20 20:17:54 +0100

    initial commit 🚀

---
+++ theme.js
@@ -0,0 +1,18 @@
+export default {
+  fonts: {
+    body: "Roboto, sans-serif",
+    monospace: '"Roboto Mono", monospace'
+  },
+  colors: {
+    text: "#333",
+    background: "white",
+    primary: "blue"
+  },
+  styles: {
+    Slide: {
+      fontFamily: "body",
+      fontSize: [3, 4, 5, '40px'],
+      justifyContent: 'center'
+    }
+  }
+};
